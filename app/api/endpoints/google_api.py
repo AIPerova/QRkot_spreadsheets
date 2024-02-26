1fa68062c2039778ec0f139f59b6310ecdeb54ea
Алёна Перова <perovaai31@yandex.ru>
2024-02-26 20:45:53 +0300

    ver 2.1

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -9,8 +9,6 @@ from app.core.google_client import get_service
 from app.core.user import current_superuser
 from app.crud.charity_project import charity_crud
 from app.schemas.charity_project import CharityProjectDB
-
-from app.crud.charity_project import charity_crud
 from app.services.google_api import (set_user_permissions,
                                      spreadsheets_create,
                                      spreadsheet_update_value)
